0750d035a6be7b1823714b877017c9d83c464130
hector lopez <hectorvmlopez@gmail.com>
2020-04-20 17:33:38 +0000

    je

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(strr, n):
     temp = list(strr)
-    if n >= 0 and n <= len(temp) -1:
+    if n >= 0 and n <= len(temp) - 1:
         del temp[n]
     return ''.join(map(str, temp))
-
