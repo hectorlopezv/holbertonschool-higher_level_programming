aa06b446c1821a18a92b81f14eb7f13cd0797f83
hector lopez <hectorvmlopez@gmail.com>
2020-04-20 15:25:22 +0000

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(strr):
     string = list(map(lambda x: ord(x) - 32 if ord(x) >= 97
-                      \and ord(x) < 123 else ord(x), strr))
+                      and ord(x) < 123 else ord(x), strr))
     for i in string:
         print('{}'.format(chr(i)), end="")
     print("")
