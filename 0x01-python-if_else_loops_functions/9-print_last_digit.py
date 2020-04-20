1aea0ac3c7fafff6c9ba63a1d97dca665d61f6e7
hector lopez <hectorvmlopez@gmail.com>
2020-04-20 15:42:57 +0000

    hector

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,3 @@ def print_last_digit(number):
     last_digit = str(number)[-1]
     print(last_digit, end="")
     return int(last_digit)
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
