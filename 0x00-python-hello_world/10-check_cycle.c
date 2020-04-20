fddb8dd328a94914cafe3aa2aec0455102adaad0
hector lopez <hectorvmlopez@gmail.com>
2020-04-20 08:21:57 +0000

    cicle detection using floyd algorithm

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *check_cycle - check for a loop in a linked list
+ *@list: list
+ *Return: return 1 for loop ,0 otherwise
+ *
+ *
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise = list;
+	listint_t *hare = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (hare != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+			return (1);
+	}
+	return (0);
+}
