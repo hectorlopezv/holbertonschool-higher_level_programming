a6a37573268797b0f76ae019de46874f145784c8
hector lopez <hectorvmlopez@gmail.com>
2020-04-21 12:43:18 +0000

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    number = 0 if len(sys.argv) - 1 == 0 else len(sys.argv) - 1
-    dot_colon = '.' if len(sys.argv) - 1 == 0 else ':' if len(sys.argv) - 1 == 1 else 's:'
-    print('{} argument{}'.format(number, dot_colon))
+    len_ag = len(sys.argv) - 1
+    number = 0 if len_ag == 0 else len_ag
+    s_or = 's' if len_ag > 1 or len_ag == 0 else ''
+    dot_colon = ':' if len_ag >= 1 else '.'
+    print('{} argument{}{}'.format(number, s_or, dot_colon))
     if len(sys.argv) - 1 == 0:
         exit()
     for i, argument in enumerate(sys.argv[1:], 1):
