bc8659e3664eadd8d2f07c4f02c7eecf5deae591
hector lopez <hectorvmlopez@gmail.com>
2020-04-21 15:40:22 +0000

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+from sys import argv
+if __name__ == '__main__':
+    len_arg = len(argv) - 1
+    operations = {'+': add, '-': sub, '*': mul, '/': div}
+    if len_arg != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    if argv[2] in operations:
+        a = int(argv[1])
+        b = int(argv[3])
+        result = operations[argv[2]](a, b)
+        print('{:d} {} {:d} = {:d}'.format(a, argv[2], b, result))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
