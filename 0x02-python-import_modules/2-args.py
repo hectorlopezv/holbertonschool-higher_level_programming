648e615df30c9c7c9e1f24a243582968ccdb5351
hector lopez <hectorvmlopez@gmail.com>
2020-04-21 12:26:59 +0000

    hector

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    argv = argv[1:]
-    count = 0
-    if len(argv) == 0:
-        print("0 arguments.")
-        pass
-    elif len(argv) == 1:
-        print("1 argument:")
-        print("1: {}".format(argv[0]))
-    else:
-        print("{} arguments:".format(len(argv)))
-        for i in argv:
-            count += 1
-            print("{}: {}".format(count, i))
+import sys
+if __name__ == '__main__':
+    number = 0 if len(sys.argv) - 1 == 0 else len(sys.argv) - 1
+    dot_colon = '.' if len(sys.argv) - 1 == 0 else ':'
+    print('{} arguments{}'.format(number, dot_colon))
+    if len(sys.argv) - 1 == 0:
+        exit()
+    for i, argument in enumerate(sys.argv[1:], 1):
+        print('{}: {}'.format(i, argument))
