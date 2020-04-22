fb104f4010b7f3ae9e5eb00f5627675bac82cd31
hector lopez <hectorvmlopez@gmail.com>
2020-04-22 22:42:35 +0000

    fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
     my_list_ = my_list[:]
     if idx < 0 or idx > len_:
         return my_list
-    my_list_[idx] = element
+    my_list_[idx] = int(element)
     return my_list_
