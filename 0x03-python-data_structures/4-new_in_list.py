e8e5f471c2e61f7006c3c50478ff8c5ce0f70bfd
hector lopez <hectorvmlopez@gmail.com>
2020-04-22 22:39:41 +0000

    fiX

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-mod = __import__('2-replace_in_list').replace_in_list
 def new_in_list(my_list, idx, element):
     len_ = len(my_list)
     my_list_ = my_list[:]
     if idx < 0 or idx > len_:
         return my_list
-    return mod(my_list_, idx, element)
+    for counter, element_ in enumerate(my_list_):
+        if counter == idx:
+            my_list_[counter] = element
+            return my_list_
