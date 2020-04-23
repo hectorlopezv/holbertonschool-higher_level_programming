7795d78851c2bc738081043e1111043b35541ff0
hector lopez <hectorvmlopez@gmail.com>
2020-04-23 02:32:16 +0000

    fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,6 +6,43 @@
  */
 int is_palindrome(listint_t **head)
 {
+	int len_list = 0, i;
+	int *temp_list;
+	listint_t *temp_head = *head, *temp_head_2 = *head;
 
+	if (head == NULL || *head == NULL)
+		return (1);
+	while (temp_head != NULL)
+	{
+		len_list++;
+		temp_head = temp_head->next;
+	}
+	temp_head = *head;
+	temp_list = malloc(len_list * sizeof(int));
+	for (i = 0; i < len_list; i++)
+	{
+		temp_list[i] = temp_head->n;
+		temp_head = temp_head->next;
+	}
+	temp_list += i - 1;
+	for (i = 0; i < len_list; i++)
+	{
+		if (*temp_list == temp_head_2->n)
+		{
+			if (i == len_list - 1)
+				continue;
+			else
+			{
+				--temp_list;
+			}
+			temp_head_2 = temp_head_2->next;
+		}
+		else
+		{
+			free(temp_list);
+			return (0);
+		}
+	}
+	free(temp_list);
 	return (1);/*palindrome*/
 }
