be5934a542251ff61ee271d35cb7f6e39f09bed9
hector lopez <hectorvmlopez@gmail.com>
2020-04-23 00:12:27 +0000

    fix

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    len_a_b = len_a - len_b
+    if len_a > len_b:
+        tuple_new = tuple_b + tuple([0 for elem in range(len_a_b)])
+        return tuple(map(lambda i, j: i + j, tuple_a, tuple_new))
+    elif len_b > len_a:
+        tuple_new = tuple_a + tuple([0 for elem in range(-1 * len_a_b)])
+        return tuple(map(lambda i, j: i + j, tuple_new, tuple_b))
+    elif len_a == len_b and len_b == 1:
+        tuple_a_new = tuple_a + tuple([0 for elem in range(1)])
+        tuple_b_new = tuple_b + tuple([0 for elem in range(1)])
+        return tuple(map(lambda i, j: i + j, tuple_a_new, tuple_b_new))
+    else:
+        return tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
