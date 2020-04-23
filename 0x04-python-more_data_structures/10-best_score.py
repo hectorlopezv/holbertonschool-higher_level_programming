7ca93809da5013c69b3778bd1e5c09068b8b475e
hector lopez <hectorvmlopez@gmail.com>
2020-04-23 08:37:44 +0000

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or not a_dictionary:
         return None
 
     def fun_(kv): return (kv[1], kv[0])
     l_ = sorted(a_dictionary.items(), reverse=True, key=fun_)
 
-    return (l_[0][1])
+    return (l_[0][0])
