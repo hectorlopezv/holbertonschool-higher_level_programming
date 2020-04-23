5a81700c72402f6b04d469c70a0dc6ac5cdc33d1
hector lopez <hectorvmlopez@gmail.com>
2020-04-23 01:40:52 +0000

    fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if not tuple_a and not tuple_b:
+        return (0, 0)
     len_a = len(tuple_a)
     len_b = len(tuple_b)
     len_a_b = len_a - len_b
