d518ad8ced1712af06de800aeda8bceb6eadceef
hector lopez <hectorvmlopez@gmail.com>
2020-04-23 00:59:42 +0000

    fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,4 +2,11 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    reduce(lambda i, j: i (if i > j) else j, my_list)
+    f = [0]
+    temp = 0
+    for element in my_list:
+        if element > temp:
+            f.pop()
+            f.append(element)
+            temp = element
+    return f[0]
