185cba657983f7e7e38da63f24b9a44c060a57dc
hector lopez molinares <hectorvmlopez@gmail.com>
2020-05-19 21:07:45 -0500

    fi

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,14 +8,14 @@ class Square:
     def __init__(self, size=0):
         """method
 
-        Keyword Arguments:
+        Arguments:
             size {int} -- size of the square
 
         Raises:
             TypeError: if size is not a int
             ValueError: if size is less than 0
         """
-        if not isinstance(int, size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
