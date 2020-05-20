6b491c31be650ac51fa7f8ef87b064d0d0473933
hector lopez molinares <hectorvmlopez@gmail.com>
2020-05-19 21:27:22 -0500

    fix

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" class of square"""
+class Square:
+    """class Square
+    """
+    def __init__(self, size=0):
+        """creates an Square object
+
+        Keyword Arguments:
+            size {int} -- size of the Square (default: {0})
+
+        Raises:
+            TypeError: if size is not int 
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """area function returns
+
+        Returns:
+            [type] -- [description]
+        """
+        return self.__size**2
