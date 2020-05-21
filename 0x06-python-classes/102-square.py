b35c2e0620af9996f2a86346564b02ce4e4694fb
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-20 22:28:06 -0500

    fix

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,105 +1,79 @@
 #!/usr/bin/python3
-"""Square class"""
+""" class of square"""
 
 
 class Square:
-    """Square class
+    """class Square
     """
-    
-    def __init__(self, size=0, position=(0,0)):
-        """constructor for Square class
+    def __init__(self, size=0):
+        """class square object creation
 
         Keyword Arguments:
-            size {int} -- size of a square (default: {0})
-            position {tuple} -- position in a x,y plane (default: {(0,0)})
+            size {int} -- size int of a variable (default: {0})
         """
+
         self.size = size
-        self.position = position
-   
-    @property    
+
+    def area(self):
+        """area function returns
+
+        Returns:
+            area -- to be the area of the function
+        """
+        return self.size**2
+
+    @property
     def size(self):
         """size getter
 
         Returns:
-            int -- return size 
+            self -- return the size
         """
         return self.__size
+
     @size.setter
-    def size(self, value):
-        """setter
+    def size(self, size):
+        """size setter
 
         Arguments:
-            value {int} -- set size with integer
+            size {int} -- integer for size
 
         Raises:
-            TypeError: if size is not a integer
-            ValueError: if size is less than 0
+            TypeError: if size is not a interger
+            ValueError: if the number is less than 0
         """
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-        
-    @property
-    def position(self):
-        """position getter
+        self.__size = size
 
-        Returns:
-            tuple -- return tuple of 2 elemetns
-        """
-        return self.__position
-    @position.setter
-    def position(self, value):
-        """position setter
+    def __eq__(self, other):
+        if self.area() == other.area():
+            return True
+        return False
 
-        Arguments:
-            value {tuple} -- tuple of 2 positions
+    def __ne__(self, other):
+        if self.area() != other.area():
+            return True
+        return False
 
-        Raises:
-            TypeError: if not a tuple or not a integer(elements) 
-        """
-        if isinstance(value, tuple) and len(value) == 2:
-            a,b = value
-            if isinstance(a, int ) and isinstance(b, int):
-                self.__position = value
-        else:
-            raise TypeError("position must be a tupe of 2 positive integer")
+    def __gt__(self, other):
+        if self.area() > other.area():
+            return True
+        return False
 
-    def area(self):
-        """are method
+    def __ge__(self, other):
+        if self.area() >= other.area():
+            return True
+        return False
 
-        Returns:
-            int -- area of a circle
-        """
-        return self.__size**2
+    def __lt__(self, other):
+        if self.area() < other.area():
+            return True
+        return False
 
-    def my_print(self):
-        """my print method
-        """
-        if self.__size == 0:
-            print()
-            return
-        for _ in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print("{}{}".format(" "*self.__position[0], "#"*self.__size))
-    
-    def __str__(self):
-        """str
-
-        Returns:
-            str -- string representation for the Square class
-        """
-        resu = ""
-        if self.__size == 0:
-            return resu
-        for _ in range(self.__position[1]):
-            resu += "\n"
-        for _ in range(self.__size):
-            resu += " "*self.__position[0]+"#"*self.__size+"\n"
-        return resu[:-1]
-
-        
-
-
+    def __le__(self, other):
+        if self.area() <= other.area():
+            return True
+        return False
