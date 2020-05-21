d3bb52b1e5aba74ef3490830d8025775cdff6ded
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-20 20:45:35 -0500

    fix

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -133,36 +133,4 @@ class SinglyLinkedList:
             prev = temp_node
             temp_node = temp_node.next_node
         prev.next_node = Node(value)
-        return
-sll = SinglyLinkedList()
-sll.sorted_insert(2)
-sll.sorted_insert(5)
-sll.sorted_insert(3)
-sll.sorted_insert(10)
-sll.sorted_insert(1)
-sll.sorted_insert(-4)
-sll.sorted_insert(-3)
-sll.sorted_insert(4)
-sll.sorted_insert(5)
-sll.sorted_insert(12)
-sll.sorted_insert(3)
-print(sll)
-
-n1 = Node(3) 
-print(n1.data)
-
-n2 = Node(-5) 
-print(n2.data)
-
-n3 = Node(4) 
-
-
-try: 
-    n4 = Node("4") 
-except Exception as e: 
-    print(e)
-
-try: 
-    n2.next_node = "Node" 
-except Exception as e:
-    print(e) 
+        return 
