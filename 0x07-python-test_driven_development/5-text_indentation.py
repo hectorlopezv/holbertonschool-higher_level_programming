1b5e64fea10fe596f194f668ca7bd9b19eeb1b61
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-26 01:41:40 -0500

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,8 +13,12 @@ def text_indentation(text):
         elif el == ' ':
             if idx < len(text) - 1:
                 if ord(text[idx + 1]) > 32 and ord(text[idx + 1]) < 127:
-                    if ord(text[idx - 1]) > 32 and ord(text[idx - 1]) < 127:
+                    if text[idx - 1] in [':', '.', '?']:
+                        continue
+                    elif ord(text[idx - 1]) > 32 and ord(text[idx - 1]) < 127:
                         retu.append(el)
         elif el != ' ':
             retu.append(el)
     print("".join(retu), end="")
+
+text_indentation("Holberton. School? How are you: John")
