65dd81833bd9b8d1d4c49898ef52a276d6fed409
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-26 01:14:38 -0500

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""
-This class prints a text with 2 new lines after each of these characters: .?:
-"""
+"""text indentation"""
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: .?:
-    Arguments:
-        text {str} -- text of testing
-    Raises:
-        TypeError: text must be a string
-    """
-    if text is None or not isinstance(text, str) or len(text) < 0:
+    """text indentation: text"""
+    if not isinstance(text, str) or text is None:
         raise TypeError("text must be a string")
-    new_line = []
-    for line in text:
-        if line not in "?.:":
-            new_line.append(line)
-        else:
-            line += "\n\n"
-            new_line.append(line)
-    new_line = "".join(new_line)
-    new_line_aux = []
-    for line in new_line.split("\n"):
-        line = line.strip()
-        new_line_aux.append(line)
-    print("\n".join(new_line_aux), end="")
+    start = 0
+    end = 0
+    retu = []
+    for idx, el in enumerate(text):
+        if el == '.' or el == '?' or el == ':':
+            end = idx + 1
+            retu.append(text[start:end] + "\n\n")
+            start = idx + 1
+        elif el == ' ':
+            if idx < len(text):
+                if ord(text[idx + 1]) >= 32 and ord(text[idx + 1]) < 127:
+                    retu.append(el)
+        elif el != ' ':
+            retu.append(el)
+    print("".join(retu))
