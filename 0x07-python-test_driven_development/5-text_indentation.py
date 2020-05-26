ad76acb8c458b77d12388397da8f9678bd5d79f2
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-26 00:43:59 -0500

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,12 +4,20 @@
 
 def text_indentation(text):
     """text indentation: text"""
-    if not isinstance(text, str):
+    if not isinstance(text, str) or test is None:
         raise TypeError("text must be a string")
     start = 0
     end = 0
+    ver = 0
     for idx, el in enumerate(text):
         if el == '.' or el == '?' or el == ':':
             end = idx + 1
             print(text[start:end] + "\n")
             start = idx + 1
+            ver = 1
+    if ver == 0:
+        print(text)
+
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("Holberton School")   
