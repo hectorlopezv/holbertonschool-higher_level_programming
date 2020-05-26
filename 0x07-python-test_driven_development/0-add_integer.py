7d354fa63c34480298f94fc4d3de857a001104ba
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-25 20:18:48 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 """
 add integer module
 this module does the addition
-of two numbers only 
+of two numbers only
 """
 
 
@@ -10,10 +10,8 @@ def add_integer(a, b=98):
     """
     add_integer: add two integers
     """
-    if not isinstance(a, (int, float)) or a != a or
-    a == float("inf") or a == float("-inf"):
+    if not isinstance(a, (int, float)) or a != a or a == float("inf") or a == float("-inf"):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or b != b or
-    b == float("inf") or b == float("-inf"):
+    if not isinstance(b, (int, float)) or b != b or b == float("inf") or b == float("-inf"):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
