12e35b2e96e6c7bfeb289f61070dda44801dacc2
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-26 16:34:34 -0500

    unit test for #6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""unit test module"""
+
+
+import unittest
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
+
+
+class testing(unittest.TestCase):
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1,2,3]) , 3 , "should be 3")
+    def test_max_integer_2(self):
+        self.assertEqual(max_integer(), None, "shoulde be None")
+    def test_max_integer_3(self):
+        with self.assertRaises(TypeError):
+            max_integer((1,2))
+if __name__ == '__main__':
+    unittest.main()
+
