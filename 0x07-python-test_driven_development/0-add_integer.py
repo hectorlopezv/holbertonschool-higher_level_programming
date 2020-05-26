61d892768004943b8044b5cfa569b9d7c1769628
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-25 20:23:36 -0500

    fix for pep8 errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,8 +10,10 @@ def add_integer(a, b=98):
     """
     add_integer: add two integers
     """
-    if not isinstance(a, (int, float)) or a != a or a == float("inf") or a == float("-inf"):
+    if not isinstance(a, (int, float)) or a != a or a == float(
+            "inf") or a == float("-inf"):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or b != b or b == float("inf") or b == float("-inf"):
+    if not isinstance(b, (int, float)) or b != b or b == float(
+            "inf") or b == float("-inf"):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
