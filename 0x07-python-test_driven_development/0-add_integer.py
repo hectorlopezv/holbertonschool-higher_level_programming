72b9069109bb6acf2152bf567fea2ac78fbfd659
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-25 20:07:44 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,21 +3,8 @@
 
 
 def add_integer(a, b=98):
-    """ Function that add to numbers"""
-    """
-    Arguments:
-        a {int, float} -- integer or float variable
-
-    Keyword Arguments:
-        b {int, float} -- integer or float variable (default: {98})
-
-    Raises:
-        TypeError: a must be an integer
-        TypeError: b must be an integer
-
-    Returns:
-        int -- return the addition for a and b
-    """
+    ''' Function that add to numbers'''
+    
     if not isinstance(a, (int, float)) or a != a or
     a == float("inf") or a == float("-inf"):
         raise TypeError("a must be an integer")
