6b0cd3610e6da5fa92213a5c05f5d0c773858064
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-25 20:11:01 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,10 +3,7 @@
 
 
 def add_integer(a, b=98):
-   """ 
-   add_integer: check for addition of two integers
-   """
-   """
+   """ add_integer: adding integers
     Arguments:
         a {int, float} -- integer or float variable
 
