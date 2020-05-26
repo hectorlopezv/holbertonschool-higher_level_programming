098dfb416c0a639fcf5c63e2dbbca7c11162f12b
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-25 22:37:01 -0500

    fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,5 +9,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    padd = "" if len(last_name) == 0 else " "
-    return "My name is " + first_name + padd + last_name
+    
+    return "My name is " + first_name + " " + last_name
