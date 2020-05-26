c69d457f5411b1096a266097e9ea029a007c4959
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-25 23:51:53 -0500

    print sqaure

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,6 +3,7 @@
 
 
 def print_square(size):
+    """ print_Square"""
     if isinstance(size, float) and size < 0:
         raise ValueError("size must be an integer")
     if not isinstance(size, int):
