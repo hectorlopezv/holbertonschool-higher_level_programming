f5d7141115f375dacd850adea1614029a931b325
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-25 20:02:17 -0500

    #0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""add integer module"""
+
+
+def add_integer(a, b=98):
+    """add_integer function
+
+    Arguments:
+        a {int, float} -- integer or float variable
+
+    Keyword Arguments:
+        b {int, float} -- integer or float variable (default: {98})
+
+    Raises:
+        TypeError: a must be an integer
+        TypeError: b must be an integer
+
+    Returns:
+        int -- return the addition for a and b
+    """
+    if not isinstance(a, (int, float)) or a != a or
+    a == float("inf") or a == float("-inf"):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)) or b != b or
+    b == float("inf") or b == float("-inf"):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
