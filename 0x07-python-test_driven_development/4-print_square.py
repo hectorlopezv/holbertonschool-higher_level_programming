02a778bbd0e3029eff1e50ad929d1533d42597f5
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-26 00:24:55 -0500

    fix for docstring

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,9 +5,9 @@
 def print_square(size):
     """function to print_Squaree"""
     if isinstance(size, float) and size < 0:
-        raise ValueError("size must be an integer")
+        raise TypeError("size must be an integer")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
-        raise TypeError("size must be >= 0")
+        raise ValueError("size must be >= 0")
     print(((("#" * size) + "\n") * size)[:-1])
