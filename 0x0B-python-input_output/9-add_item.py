f8de6e43df3fb66cdb7c18ca00ce1f39bd451c62
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 16:52:56 -0500

    fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """add item"""
-if __name__ == '__main__':
-    import sys
 
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '8-load_from_json_file').load_from_json_file
+import sys
 
-    try:
-        info = load_from_json_file('add_item.json')
-    except BaseException:
-        info = []
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-    finally:
-        save_to_json_file(info + sys.argv[1:], 'add_item.json')
+try:
+    info = load_from_json_file('add_item.json')
+except BaseException:
+    info = []
+
+finally:
+    save_to_json_file(info + sys.argv[1:], 'add_item.json')
