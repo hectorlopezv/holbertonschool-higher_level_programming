46529ef4e1afc34304e19199cf799ce0a843e8b4
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 15:33:26 -0500

    #7

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -4,5 +4,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """object to json"""
+    with open(filename, 'w') as f:
 
-    return json.dump(my_obj, filename)
+        return json.dump(my_obj, f)
