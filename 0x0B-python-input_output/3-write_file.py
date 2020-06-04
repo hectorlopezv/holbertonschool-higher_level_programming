e8d06072667d534b2e77462276f18a036da5bd50
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 15:10:49 -0500

    fix

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """write_file"""
-    with open(filename, 'w', enconde='utf8') as f:
-        f.write(text)
+    with open(filename, 'w') as f:
+        return f.write(text)
