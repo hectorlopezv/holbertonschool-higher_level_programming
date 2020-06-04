98549acf86d3dffef7209581dba4505d5bf06a6d
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 13:38:51 -0500

    cleaning repo

--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def write_file(filename="", text=""):
-    lines = 0
-    with open(filename, 'w') as f:
-        lines = f.write(text)
-    return lines
--- 0x0B-python-input_output/8-load_from_json_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import json
-
-
-def load_from_json_file(filename):
-    with open(filename, 'r') as f:
-        data = json.loads(f.readline())
-        return data
