e6f596b415918a6b74d47d4bffeabf55c3deeafe
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 16:56:32 -0500

    fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-'''Module for task 9'''
+"""Load, add, save"""
+
+
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+arguements = sys.argv[1:]
 try:
-    data = load_from_json_file('add_item.json')
-except:
-    data = []
-
-save_to_json_file(data + sys.argv[1:], 'add_item.json')
+    ls = load_from_json_file(filename)
+    list_ = ls + arguements
+    save_to_json_file(list_, filename)
+except Exception:
+    save_to_json_file(arguements, filename)
