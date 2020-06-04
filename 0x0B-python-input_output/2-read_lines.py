fc64dd8801ff7bcd3662cab770109ad0238323e5
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 14:57:50 -0500

    fix

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -9,7 +9,7 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, 'r') as f:
         if nb_lines == 0:
             print(f.read(), end="")
-        elif nb_lines <= number_of_lines(filename) and nb_lines > 0:
+        elif nb_lines <= number_of_lines(filename):
             for counter, line in enumerate(f):
                 if counter < nb_lines:
                     print(line, end="")
