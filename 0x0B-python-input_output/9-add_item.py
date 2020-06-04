c1bbd31a9738c122274ee6cd540e125a67d63d1b
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 16:50:54 -0500

    fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,7 +2,6 @@
 """add item"""
 if __name__ == '__main__':
     import sys
-    import json
 
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = __import__(
