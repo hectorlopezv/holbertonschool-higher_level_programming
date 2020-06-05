c45be1109e57ca8402dae03baaf9bea03f210a22
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 19:32:46 -0500

    fix

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""100-append"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, 'r') as f:
+        file_tmp = f.readlines()
+    with open(filename, 'w') as f:
+        for line in file_tmp:
+            if search_string in line:
+                f.write(line)
+                f.write(new_string)
+            else:
+                f.write(line)
