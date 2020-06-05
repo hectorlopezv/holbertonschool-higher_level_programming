782c385eff23c9c746ef697ee6f1627d9add8625
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 21:09:06 -0500

    fi2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-""" stats: script to get stats from generated http requests """
+'''Module for log parsing script.'''
 import sys
-import traceback
-import time
 
-codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total = 0
-length = 0
+if __name__ == "__main__":
+size = [0]
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
+def check_match(line):
+    '''Checks for regexp match in line.'''
+    try:
+        line = line[:-1]
+        words = line.split(" ")
+        size[0] += int(words[-1])
+        code = int(words[-2])
+        if code in codes:
+            codes[code] += 1
+    except:
+        pass
 
-def print_info():
-    """ prints info about parsed made so far """
-    print("File size: {:d}".format(length))
-    for x in sorted(codes.keys()):
-        if codes[x] != 0:
-            print("{:s}: {:d}".format(x, codes[x]))
-
+def print_stats():
+    '''Prints accumulated statistics.'''
+    print("File size: {}".format(size[0]))
+    for k in sorted(codes.keys()):
+        if codes[k]:
+            print("{}: {}".format(k, codes[k]))
+i = 1
 try:
     for line in sys.stdin:
-        length += int(line.split()[8])
-        codes[line.split()[7]] += 1
-        total += 1
-        if total % 10 == 0:
-            print_info()
-except KeyboardInterrupt as e:
-    print_info()
+        check_match(line)
+        if i % 10 == 0:
+            print_stats()
+        i += 1
+except KeyboardInterrupt:
+    print_stats()
+    raise
+print_stats()
