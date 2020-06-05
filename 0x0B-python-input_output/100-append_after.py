37ae5a4d1eed533dcfa8c5cb08f51be0543f193a
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-04 19:33:34 -0500

    fix

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,8 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """append_after"""
+
     with open(filename, 'r') as f:
         file_tmp = f.readlines()
     with open(filename, 'w') as f:
