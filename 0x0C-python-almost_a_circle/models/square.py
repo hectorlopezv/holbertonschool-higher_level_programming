3d8575646fd754563f6b4593967fd8e476dd4660
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-06-09 21:28:01 -0500

    pep8 clean

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """constructor for square class"""
-        super().__init__(size, size, x, y , id)
+        super().__init__(size, size, x, y, id)
