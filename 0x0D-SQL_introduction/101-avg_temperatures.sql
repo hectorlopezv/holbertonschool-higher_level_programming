55301600179f8e3a22dfda0c42a808581a4f6f88
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-07-17 17:18:12 -0500

    1 advanced

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,8 @@
 -- Average temperature
-SELECT city, AVG(value) AS avg_temp 
+SELECT city, AVG(value) AS avg_temp, month
 FROM temperatures
+WHERE month = 8 or month = 7
 GROUP BY city
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC
+LIMIT 3;
+;
