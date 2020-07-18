7d03f3acfd454b4468f7ef206a093f1ab7cfe164
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-07-17 19:39:45 -0500

    creating table states , id primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,5 @@
 -- creating states tables
 CREATE DATABASE  IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS state(
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256),
