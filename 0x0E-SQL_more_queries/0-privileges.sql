c421ee05b4c5483e4b26cae9b3a9a4f04e8bef22
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-07-17 19:11:25 -0500

    first commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- set privilages for a user 
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
