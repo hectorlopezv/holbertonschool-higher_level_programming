84f2b8088fbd38b3b84e6fb72f14fe367636cc11
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-07-17 21:47:52 -0500

    10 genre

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Genere id show
+SELECT tv_show_genres.genre_id, tv_shows.title 
+FROM tv_show_genres 
+JOIN tv_shows 
+ON tv_shows.id = tv_show_genres.show_id  
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
