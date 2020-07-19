850dbcd8e1acf9a5c8ce908094cac5b63e683886
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-07-19 06:43:16 -0500

    fix

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,11 @@
+-- rating
+SELECT tv_genres.name , SUM(tv_show_ratings.rate) AS rating
+FROM tv_show_genres 
+JOIN tv_shows 
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_show_ratings 
+ON tv_show_ratings.show_id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
