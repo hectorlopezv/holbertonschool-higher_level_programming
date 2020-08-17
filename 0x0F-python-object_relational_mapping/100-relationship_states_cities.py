fcc0f838ef2c4fafc5ce74eda4b4d6222bad3435
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-17 17:00:58 -0500

    understading how relationships works in mysql and sql alchemy

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-"""how to use mysql client and compare it so sql alchemy"""
-if __name__ == '__main__':
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker, Session, scoped_session
-    from sqlalchemy.ext.declarative import declarative_base
-    import sys
-    from sqlalchemy import Column, String, ForeignKey, Integer
-    from relationship_state import Base, State
-    from relationship_city import City
+"""Create state California"""
 
-    args_ = sys.argv[1:]
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.format(
-        args_[0], args_[1], 'localhost', args_[2]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)  # create like database..
+if __name__ == "__main__":
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    session_factory = sessionmaker(bind=engine,
-                                   expire_on_commit=False)
-    Session = scoped_session(session_factory)
-
+    Session = sessionmaker(bind=engine)
     session = Session()
-
-    state_ = State(name="Calfornia")
-    session.add(state_)
+    new_state = State(name="California")
+    session.add(new_state)
     session.commit()
-
-    city_ = City(name="San Francisco", state_id=state_.id)
-
-    session.add(city_)
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    session.add(new_city)
     session.commit()
-
-    # print(state_info.cities)
-    # print(state_info.cities[0].name)
     session.close()
