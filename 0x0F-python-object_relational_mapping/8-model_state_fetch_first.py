5032443c111021c766044817113e4711f0cbbebb
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-17 14:48:03 -0500

    adding exercice number 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""how to use mysql client and compare it so sql alchemy"""
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker, Session, scoped_session
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import Column, Integer, String, DateTime
+    import sys
+    from model_state import Base, State
+
+    args_ = sys.argv[1:]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.format(
+        args_[0], args_[1], 'localhost', args_[2]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)  # create like database..
+
+    session_factory = sessionmaker(bind=engine,
+                                   expire_on_commit=False)
+    Session = scoped_session(session_factory)
+
+    session = Session()
+
+    obj_ = session.query(State).first()
+
+    # print(result)
+
+    print("{}: {}".format(obj_.id, obj_.name))
