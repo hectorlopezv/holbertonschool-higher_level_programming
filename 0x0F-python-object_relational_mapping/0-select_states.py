dfe69fa14242fff93b100e6e11db3cc412a38fdc
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 20:57:27 -0500

    adding first exercise

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-#how to use mysql client and compare it so sql alchemy      
+"""how to use mysql client and compare it so sql alchemy"""
 if __name__ == '__main__':
     import sys
     import MySQLdb
     args_ = sys.argv
-    #print(args_)
-    
-    db=MySQLdb.connect(user=args_[1], passwd=args_[2], db=args_[3])
+    # print(args_)
+
+    db = MySQLdb.connect(user=args_[1], passwd=args_[2], db=args_[3])
     c = db.cursor()
     c.execute("SELECT * FROM states")
     for row in c.fetchall():
