7004d86685db98d9630780ade1fea320479872e7
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 23:00:16 -0500

    adding model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 """how to use mysql client and compare it so sql alchemy"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session, scoped_session, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+Base = declarative_base() #used to make classes a table.....fml
+
+class State(Base):
+    """state class"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
