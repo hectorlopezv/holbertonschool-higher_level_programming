b4423e6955cca55b19188b37568d66edef08ca51
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-17 15:36:00 -0500

    how to update row using either object atribute or using the .update({}}) method

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""how to use mysql client and compare it so sql alchemy"""
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker, Session, scoped_session
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import Column, Integer, String, DateTime
+    import sys
+    from model_state import Base, State
+
+    args_ = sys.argv[1:]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.format(
+        args_[0], args_[1], 'localhost', args_[2]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)  # create like database..
+
+    session_factory = sessionmaker(bind=engine,
+                                   expire_on_commit=False)
+    Session = scoped_session(session_factory)
+
+    session = Session()
+    session.query(State).filter(State.id == 2).\
+        update({State.name: "New Mexico"}, synchronize_session=False)
+    session.commit()
+    session.close()
