3cd09b4c96bd91cb9a359d19e2a1998df73e2783
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-17 17:02:47 -0500

    understading how relationships works in mysql and sql alchemy

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -13,4 +13,9 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref=backref('state', cascade='all, delete'), single_parent=True)
+    cities = relationship(
+        'City',
+        backref=backref(
+            'state',
+            cascade='all, delete'),
+        single_parent=True)
