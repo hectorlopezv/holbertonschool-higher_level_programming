55f193d10831b54e9d971f9ec94426d730fdbe85
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 21:23:18 -0500

    adding 1# filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,10 +9,11 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=args_[1], passwd=args_[2], db=args_[3])
     c = db.cursor()
     c.execute(
-        "SELECT states.id, states.name FROM states WHERE states.name \
-            LIKE 'N%' \
-            ORDER BY states.id ASC")
+        """
+        SELECT states.id, states.name FROM states 
+        WHERE states.name REGEXP '^N' ORDER BY states.id ASC""")
     result = c.fetchall()
+    print(result)
     if result:
         for row in c.fetchall():
             print(row)
