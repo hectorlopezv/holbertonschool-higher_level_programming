977e61b5b1b76d1e82856ee62f6723fde5a9b941
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-17 16:51:35 -0500

    understading how relationships works in mysql and sql alchemy

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -4,13 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, scoped_session, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy import Column, String, ForeignKey, Integer
-from relationship_state import Base, State
+Base = declarative_base()  # used to make classes a table.....fml
 
 
-class City(Base):
+class State(Base):
     """state class"""
-    __tablename__ = "cities"
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
