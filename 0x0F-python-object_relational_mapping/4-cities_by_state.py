0b5417d811a98eaada64399ed46a5ef22fe35883
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 22:22:44 -0500

    adding join #4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""how to use mysql client and compare it so sql alchemy"""
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+    args_ = sys.argv
+    # print(args_)
+
+    db = MySQLdb.connect(user=args_[1], passwd=args_[2], db=args_[3])
+    c = db.cursor()
+    str_ = """
+SELECT cities.id, cities.name, states.name FROM states 
+JOIN cities ON states.id = cities.state_id"""
+    c.execute(str_)
+    query_rows = c.fetchall()
+    for row in query_rows:
+        print(row)
+    c.close()
+    db.close()
