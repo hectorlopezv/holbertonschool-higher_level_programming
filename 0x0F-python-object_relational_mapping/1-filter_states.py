9a1d1b449778447d5874c060f956b4c606103123
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 21:03:22 -0500

    adding 1# filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""how to use mysql client and compare it so sql alchemy"""
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+    args_ = sys.argv
+    # print(args_)
+
+    db = MySQLdb.connect(user=args_[1], passwd=args_[2], db=args_[3])
+    c = db.cursor()
+    c.execute(
+        "SELECT states.id, states.name FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
+    for row in c.fetchall():
+        print(row)
