9262957d3ef5654a8fce4279e726593cb0e02b23
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-16 22:43:52 -0500

    adding exercise number 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,16 +7,14 @@ if __name__ == '__main__':
     # print(args_)
 
     db = MySQLdb.connect(user=args_[1], passwd=args_[2], db=args_[3])
-    c = db.cursor()
-    str_ = """SELECT cities.name FROM cities 
-    WHERE cities.state_id IN ( SELECT states.id FROM states WHERE states.name = '{}')
-    """.format(args_[4])
-    c.execute(str_)
-    query_rows = c.fetchall()
-    print("{}{}{}".format('', '', query_rows[0][0]), end="")
-    if query_rows:
-        for row in query_rows[1:]:
-            print("{}{}{}".format(',', ' ', row[0]), end="")
-        print()
-    c.close()
-    db.close()
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name FROM cities
+        LEFT JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC;
+        """, (argv[4],))
+    query_rows = cur.fetchall()
+    print(", ".join([row[1] for row in query_rows]))
+    cur.close()
+    conn.close()
