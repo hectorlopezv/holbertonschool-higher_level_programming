2e218a71e8d9ecc9d336ed6e3a3e78486e405cdf
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-22 15:24:05 -0500

    get methods accepted

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #curl only methods
-res=$(curl -is 0.0.0.0:5000/route_4 | awk '{if (NR == 3) print $0}')
-echo "${res[@]:7}"
+res=$(curl -is 0.0.0.0:5000/route_4 | awk '{if (NR == 3) print $0}');echo "${res[@]:7}"
