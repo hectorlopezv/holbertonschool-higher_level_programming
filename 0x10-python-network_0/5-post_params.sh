12dd4695bc4541cc85478eb312817dee2890456a
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-22 15:48:43 -0500

    curl post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post request
-curl -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
+curl -sd 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
