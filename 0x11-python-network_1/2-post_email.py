7896f97cd8362b6da8af80a7a3d110c6e2dd166c
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 17:13:28 -0500

    fixing pep

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 """python request module"""
-from urllib import request
+from urllib import request, parse
 import sys
 
-if __name__ == '__main__':
 
-    url = sys.argv[1]
-    email = sys.argv[2]
-    values = {'email': email}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')  # data should be bytes
-
-    request = urllib.request.Request(url, data)
-
-    with urllib.request.urlopen(request) as response:
-        html = response.read()
-        print(html.decode('utf-8'))
+if __name__ == "__main__":
+    data = parse.urlencode({"email": sys.argv[2]}).encode()
+    with request.urlopen(sys.argv[1], data) as response:
+        print(response.read().decode('utf-8'))
