d5a1a0efbf29fdca56196a52a9061dc9490324ca
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 17:26:58 -0500

    fixing pep

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """status python network"""
-
-
 import requests
 import sys
-url = sys.argv[1]
-email = sys.argv[2]
-r = requests.post(url, data = {'email':email})
-print(r.text)
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data = {'email':email})
+    print(r.text)
