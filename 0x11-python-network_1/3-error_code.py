843e7b1f88f8f6bb198c2fed961a6344eace0f43
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 17:19:45 -0500

    fixing pep

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,11 +3,11 @@
 from urllib import request, parse, error
 import sys
 
-
-url = sys.argv[1]
-try:
-    with request.urlopen(url) as response:
-        html = response.read()
-        print(html.decode('utf-8'))
-except error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
