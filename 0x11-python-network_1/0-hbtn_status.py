e4a854e4bc1834fb61889c8a20d99437a8569e79
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 09:37:21 -0500

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""python request module"""
+
+
+import urllib.request
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+    print(type(html))
+    print(
+        """
+Body response:$
+    - type: {}$
+    - content: {}$
+    - utf8 content: {}$)
+""".format(type(html), html, html.decode('utf-8'))
+    )
