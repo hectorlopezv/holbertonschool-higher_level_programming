045ff1e4f727d35d9c9949ef751ff1745929ac2b
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 15:04:08 -0500

    fixing pep #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,13 @@
 """python request module"""
 
 
-import urllib.request
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
+from urllib import requests
+req = request.Request('https://intranet.hbtn.io/status')
+with request.urlopen(req) as response:
     html = response.read()
     print(
         """Body response:
-    - type: {}
+        - type: {}
     - content: {}
     - utf8 content: {})""".format(type(html), html, html.decode('utf-8'))
     )
