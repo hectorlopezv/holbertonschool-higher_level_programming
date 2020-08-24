7735df3dae833de5f1838e72c9932828b37d3786
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 17:31:27 -0500

    fixing pep

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """status python network"""
-
-
 import requests
 import sys
-url = 'http://0.0.0.0:5000/search_user'
 
-q = sys.argv[1] if len(sys.argv) >= 2 else ""
-r = requests.post(url, data = {'q':q})
-r.raise_for_status()
-result = r.json()
-if not result:
-    print("No result")
-else:
-    print("[{}] {}".format(result.get("id"), result.get("name")))
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    q = sys.argv[1] if len(sys.argv) >= 2 else ""
+    r = requests.post(url, data = {'q':q})
+    r.raise_for_status()
+    result = r.json()
+    if not result:
+        print("No result")
+    else:
+        print("[{}] {}".format(result.get("id"), result.get("name")))
