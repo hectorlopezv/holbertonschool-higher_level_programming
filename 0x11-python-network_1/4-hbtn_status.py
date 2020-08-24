dd58dc1f4df737095f17b9d3a897882c091a9673
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 17:24:00 -0500

    fixing pep

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ import requests
 url = 'https://intranet.hbtn.io/status'
 r = requests.get(url)
 print("""Body response:\n\t- type: {}\n\t- content: {}"""
-      .format(type(r.text), r.text) )
+      .format(type(r.text), r.text))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """status python network"""
-
-
 import requests
 import sys
-url = sys.argv[1]
 
-r = requests.get(url)
-print(r.headers['X-Request-Id'])
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
