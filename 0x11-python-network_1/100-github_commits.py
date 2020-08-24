1909df59021efdf995cc5f1586e75f60d44411ca
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-24 17:30:32 -0500

    fixing pep

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """status python network"""
-
-
 import requests
 import sys
 
-user = sys.argv[1]
-repo = sys.argv[2]
 
-url = 'https://api.github.com/repos/{}/{}/commits?per_page=10'.format(user, repo)
-headers = {'Accept': 'application/vnd.github.v3+json'}
-result = requests.get(url, headers=headers)
+if __name__ == '__main__':
+    user = sys.argv[1]
+    repo = sys.argv[2]
 
-try:
-    result.raise_for_status()
-    res_json = result.json()
-    for el in res_json:
+    url = 'https://api.github.com/repos/{}/{}/commits?per_page=10'.format(user, repo)
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    result = requests.get(url, headers=headers)
 
-        author_name = el.get("commit").get("author").get("name")
-        author_commit_sha = el.get("sha")
-        print(str(author_commit_sha) + ": " +  str(author_name) )
-except requests.exceptions.RequestException as e:
-    print("None")
+    try:
+        result.raise_for_status()
+        res_json = result.json()
+        for el in res_json:
+
+            author_name = el.get("commit").get("author").get("name")
+            author_commit_sha = el.get("sha")
+            print(str(author_commit_sha) + ": " +  str(author_name) )
+    except requests.exceptions.RequestException as e:
+        print("None")
