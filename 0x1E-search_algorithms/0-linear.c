fcdd668fb2e97f41ee67cc2fcbeffacdaf8dba1c
Hector Lopez <hectorvmlopez@gmail.com>
2020-08-26 19:10:44 -0500

    first commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search
+ *@array: array of intergers
+ *@size: size of the array
+ *@value: value to search
+ *Return: -1 if not found else value
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	int i;
+
+	for (i = 0; (size_t)i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
